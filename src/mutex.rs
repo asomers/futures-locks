asomers/futures-locks

commit 76b453175756c3171bea69c4093ae8bd2fd7c838

Fix deadlocks when dropping a Future before polling it
Creating a Future and then dropping it without ever polling it is weird
but legal behavior. Also, polling it at least once and then dropping it
before it ever returns Async::Ready is also legal.  Handle these
problems by implementing Drop for all futures-locks Future types.

Fixes #1

src/mutex.rs
@@ -36,13 +36,47 @@ impl<T: ?Sized> DerefMut for MutexGuard<T> {
 
 /// A `Future` representation a pending `Mutex` acquisition.
 pub struct MutexFut<T: ?Sized> {
+    /// Has this Future already acquired the Mutex?
+    acquired: bool,
     receiver: Option<oneshot::Receiver<()>>,
     mutex: Mutex<T>,
 }
 
 impl<T: ?Sized> MutexFut<T> {
     fn new(rx: Option<oneshot::Receiver<()>>, mutex: Mutex<T>) -> Self {
-        MutexFut{receiver: rx, mutex}
+        MutexFut{acquired: false, receiver: rx, mutex}
+    }
+}
+
+impl<T: ?Sized> Drop for MutexFut<T> {
+    fn drop(&mut self) {
+        if ! self.acquired {
+            if let Some(ref mut rx) = &mut self.receiver {
+                rx.close();
+                // TODO: futures-0.2.0 introduces a try_recv method that is
+                // better to use here than poll.  Use it after upgrading to
+                // futures >= 0.2.0
+                match rx.poll() {
+                    Ok(Async::Ready(())) => {
+                        // This future received ownership of the mutex, but got
+                        // dropped before it was ever polled.  Release the
+                        // mutex.
+                        self.mutex.unlock()
+                    },
+                    Ok(Async::NotReady) => {
+                        // Dropping the Future before it acquires the Mutex is
+                        // equivalent to cancelling it.
+                    },
+                    Err(oneshot::Canceled) => {
+                        // Never received ownership of the mutex
+                    }
+                }
+            } else {
+                // Even though the future was immediately ready, it never got
+                // polled.
+                self.mutex.unlock();
+            }
+        }
     }
 }
 
@@ -52,6 +86,7 @@ impl<T> Future for MutexFut<T> {
 
     fn poll(&mut self) -> Poll<Self::Item, Self::Error> {
         if self.receiver.is_none() {
+            self.acquired = true;
             Ok(Async::Ready(MutexGuard{mutex: self.mutex.clone()}))
         } else {
             match self.receiver.poll() {
@@ -62,6 +97,7 @@ impl<T> Future for MutexFut<T> {
                 // Fut retains a clone of the RwLock
                 Err(_) => unreachable!(),
                 Ok(Async::Ready(_)) => {
+                    self.acquired = true;
                     Ok(Async::Ready(MutexGuard{mutex: self.mutex.clone()}))
                 }
             }

src/rwlock.rs
@@ -56,13 +56,47 @@ impl<T: ?Sized> Drop for RwLockWriteGuard<T> {
 
 /// A `Future` representation a pending `RwLock` shared acquisition.
 pub struct RwLockReadFut<T: ?Sized> {
+    /// Has this Future already acquired the RwLock?
+    acquired: bool,
     receiver: Option<oneshot::Receiver<()>>,
     rwlock: RwLock<T>,
 }
 
 impl<T: ?Sized> RwLockReadFut<T> {
     fn new(rx: Option<oneshot::Receiver<()>>, rwlock: RwLock<T>) -> Self {
-        RwLockReadFut{receiver: rx, rwlock}
+        RwLockReadFut{acquired: false, receiver: rx, rwlock}
+    }
+}
+
+impl<T: ?Sized> Drop for RwLockReadFut<T> {
+    fn drop(&mut self) {
+        if ! self.acquired {
+            if let Some(ref mut rx) = &mut self.receiver {
+                rx.close();
+                // TODO: futures-0.2.0 introduces a try_recv method that is
+                // better to use here than poll.  Use it after upgrading to
+                // futures >= 0.2.0
+                match rx.poll() {
+                    Ok(Async::Ready(())) => {
+                        // This future received ownership of the lock, but got
+                        // dropped before it was ever polled.  Release the
+                        // lock.
+                        self.rwlock.unlock_reader()
+                    },
+                    Ok(Async::NotReady) => {
+                        // Dropping the Future before it acquires the lock is
+                        // equivalent to cancelling it.
+                    },
+                    Err(oneshot::Canceled) => {
+                        // Never received ownership of the lock
+                    }
+                }
+            } else {
+                // Even though the future was immediately ready, it never got
+                // polled.
+                self.rwlock.unlock_reader();
+            }
+        }
     }
 }
 
@@ -72,6 +106,7 @@ impl<T> Future for RwLockReadFut<T> {
 
     fn poll(&mut self) -> Poll<Self::Item, Self::Error> {
         if self.receiver.is_none() {
+            self.acquired = true;
             Ok(Async::Ready(RwLockReadGuard{rwlock: self.rwlock.clone()}))
         } else {
             match self.receiver.poll() {
@@ -82,7 +117,10 @@ impl<T> Future for RwLockReadFut<T> {
                 // Fut retains a clone of the RwLock
                 Err(_) => unreachable!(),
                 Ok(Async::Ready(_)) => {
-                    Ok(Async::Ready(RwLockReadGuard{rwlock: self.rwlock.clone()}))
+                    self.acquired = true;
+                    Ok(Async::Ready(
+                        RwLockReadGuard{ rwlock: self.rwlock.clone() }
+                    ))
                 }
             }
         }
@@ -91,13 +129,47 @@ impl<T> Future for RwLockReadFut<T> {
 
 /// A `Future` representation a pending `RwLock` exclusive acquisition.
 pub struct RwLockWriteFut<T: ?Sized> {
+    /// Has this Future already acquired the RwLock?
+    acquired: bool,
     receiver: Option<oneshot::Receiver<()>>,
     rwlock: RwLock<T>,
 }
 
 impl<T: ?Sized> RwLockWriteFut<T> {
     fn new(rx: Option<oneshot::Receiver<()>>, rwlock: RwLock<T>) -> Self {
-        RwLockWriteFut{receiver: rx, rwlock}
+        RwLockWriteFut{acquired: false, receiver: rx, rwlock}
+    }
+}
+
+impl<T: ?Sized> Drop for RwLockWriteFut<T> {
+    fn drop(&mut self) {
+        if ! self.acquired {
+            if let Some(ref mut rx) = &mut self.receiver {
+                rx.close();
+                // TODO: futures-0.2.0 introduces a try_recv method that is
+                // better to use here than poll.  Use it after upgrading to
+                // futures >= 0.2.0
+                match rx.poll() {
+                    Ok(Async::Ready(())) => {
+                        // This future received ownership of the lock, but got
+                        // dropped before it was ever polled.  Release the
+                        // lock.
+                        self.rwlock.unlock_writer()
+                    },
+                    Ok(Async::NotReady) => {
+                        // Dropping the Future before it acquires the lock is
+                        // equivalent to cancelling it.
+                    },
+                    Err(oneshot::Canceled) => {
+                        // Never received ownership of the lock
+                    }
+                }
+            } else {
+                // Even though the future was immediately ready, it never got
+                // polled.
+                self.rwlock.unlock_writer();
+            }
+        }
     }
 }
 
@@ -107,6 +179,7 @@ impl<T> Future for RwLockWriteFut<T> {
 
     fn poll(&mut self) -> Poll<Self::Item, Self::Error> {
         if self.receiver.is_none() {
+            self.acquired = true;
             Ok(Async::Ready(RwLockWriteGuard{rwlock: self.rwlock.clone()}))
         } else {
             match self.receiver.poll() {
@@ -117,7 +190,10 @@ impl<T> Future for RwLockWriteFut<T> {
                 // Fut retains a clone of the RwLock
                 Err(_) => unreachable!(),
                 Ok(Async::Ready(_)) => {
-                    Ok(Async::Ready(RwLockWriteGuard{rwlock: self.rwlock.clone()}))
+                    self.acquired = true;
+                    Ok(Async::Ready(
+                        RwLockWriteGuard{rwlock: self.rwlock.clone()}
+                    ))
                 }
             }
         }

tests/test.rs
@@ -14,6 +14,42 @@ mod mutex {
 use super::*;
 use futures::{Future, Stream};
 
+// When an owned but not yet polled Mutex future is dropped, it should
+// relinquish ownership.  If not, deadlocks may result.
+#[test]
+fn drop_before_poll() {
+    let mutex = Mutex::<u32>::new(0);
+
+    let _ = current_thread::block_on_all(lazy(|| {
+        let mut fut1 = mutex.lock(); // fut1 immediately gets ownership
+        let guard1 = fut1.poll().unwrap();
+        let mut fut2 = mutex.lock();
+        assert!(!fut2.poll().unwrap().is_ready());
+        drop(guard1);                // ownership transfers to fut2
+        drop(fut1);
+        drop(fut2);                  // relinquish ownership
+        let mut fut3 = mutex.lock(); // fut3 immediately gets ownership
+        assert!(fut3.poll().unwrap().is_ready());
+        future::ok::<(), ()>(())
+    }));
+}
+
+// When an owned but not yet polled Mutex future is dropped, it should
+// relinquish ownership.  If not, deadlocks may result.
+#[test]
+fn drop_immediately_ready_before_poll() {
+    let mutex = Mutex::<u32>::new(0);
+
+    let _ = current_thread::block_on_all(lazy(|| {
+        let fut1 = mutex.lock();        // fut1 immediately gets ownership
+        let mut fut2 = mutex.lock();
+        assert!(!fut2.poll().unwrap().is_ready());
+        drop(fut1);                     // ownership transfers to fut2
+        assert!(fut2.poll().unwrap().is_ready());
+        future::ok::<(), ()>(())
+    }));
+}
+
 // Mutably dereference a uniquely owned Mutex
 #[test]
 fn get_mut() {
@@ -126,6 +162,78 @@ fn try_unwrap_multiply_referenced() {
 mod rwlock {
 use super::*;
 use futures::{Future, Stream};
+
+// When an exclusively owned but not yet polled RwLock future is dropped, it
+// should relinquish ownership.  If not, deadlocks may result.
+#[test]
+fn drop_exclusive_before_poll() {
+    let rwlock = RwLock::<u32>::new(42);
+
+    let _ = current_thread::block_on_all(lazy(|| {
+        let mut fut1 = rwlock.read();   // fut1 immediately gets ownership
+        let guard1 = fut1.poll().unwrap();
+        let mut fut2 = rwlock.write();
+        assert!(!fut2.poll().unwrap().is_ready());
+        drop(guard1);
+        drop(fut1);                     // ownership transfers to fut2
+        drop(fut2);                     // relinquish ownership
+        let mut fut3 = rwlock.read();   // fut3 immediately gets ownership
+        assert!(fut3.poll().unwrap().is_ready());
+        future::ok::<(), ()>(())
+    }));
+}
+
+// When an exclusively owned but not yet polled RwLock future is dropped, it
+// should relinquish ownership.  If not, deadlocks may result.
+#[test]
+fn drop_exclusive_immediately_ready_before_poll() {
+    let rwlock = RwLock::<u32>::new(42);
+
+    let _ = current_thread::block_on_all(lazy(|| {
+        let fut1 = rwlock.write();      // fut1 immediately gets ownership
+        let mut fut2 = rwlock.read();
+        assert!(!fut2.poll().unwrap().is_ready());
+        drop(fut1);                     // ownership transfers to fut2
+        assert!(fut2.poll().unwrap().is_ready());
+        future::ok::<(), ()>(())
+    }));
+}
+
+// When an nonexclusively owned but not yet polled RwLock future is dropped, it
+// should relinquish ownership.  If not, deadlocks may result.
+#[test]
+fn drop_shared_before_poll() {
+    let rwlock = RwLock::<u32>::new(42);
+
+    let _ = current_thread::block_on_all(lazy(|| {
+        let mut fut1 = rwlock.write();  // fut1 immediately gets ownership
+        let guard1 = fut1.poll().unwrap();
+        let mut fut2 = rwlock.read();
+        assert!(!fut2.poll().unwrap().is_ready());
+        drop(guard1);
+        drop(fut1);                     // ownership transfers to fut2
+        drop(fut2);                     // relinquish ownership
+        let mut fut3 = rwlock.write();  // fut3 immediately gets ownership
+        assert!(fut3.poll().unwrap().is_ready());
+        future::ok::<(), ()>(())
+    }));
+}
+
+// When an nonexclusively owned but not yet polled RwLock future is dropped, it
+// should relinquish ownership.  If not, deadlocks may result.
+#[test]
+fn drop_shared_immediately_ready_before_poll() {
+    let rwlock = RwLock::<u32>::new(42);
+
+    let _ = current_thread::block_on_all(lazy(|| {
+        let fut1 = rwlock.read();       // fut1 immediately gets ownership
+        let mut fut2 = rwlock.write();
+        assert!(!fut2.poll().unwrap().is_ready());
+        drop(fut1);                     // ownership transfers to fut2
+        assert!(fut2.poll().unwrap().is_ready());
+        future::ok::<(), ()>(())
+    }));
+}
 
 // Mutably dereference a uniquely owned RwLock
 #[test]

commit 14eba6b08dc78da47c032fc514cbc25569fa1c33

Restore 1.23.0 compatibility, broken by 76b4531

src/mutex.rs
@@ -51,7 +51,7 @@ impl<T: ?Sized> MutexFut<T> {
 impl<T: ?Sized> Drop for MutexFut<T> {
     fn drop(&mut self) {
         if ! self.acquired {
-            if let Some(ref mut rx) = &mut self.receiver {
+            if let &mut Some(ref mut rx) = &mut self.receiver {
                 rx.close();
                 // TODO: futures-0.2.0 introduces a try_recv method that is
                 // better to use here than poll.  Use it after upgrading to

src/rwlock.rs
@@ -71,7 +71,7 @@ impl<T: ?Sized> RwLockReadFut<T> {
 impl<T: ?Sized> Drop for RwLockReadFut<T> {
     fn drop(&mut self) {
         if ! self.acquired {
-            if let Some(ref mut rx) = &mut self.receiver {
+            if let &mut Some(ref mut rx) = &mut self.receiver {
                 rx.close();
                 // TODO: futures-0.2.0 introduces a try_recv method that is
                 // better to use here than poll.  Use it after upgrading to
@@ -144,7 +144,7 @@ impl<T: ?Sized> RwLockWriteFut<T> {
 impl<T: ?Sized> Drop for RwLockWriteFut<T> {
     fn drop(&mut self) {
         if ! self.acquired {
-            if let Some(ref mut rx) = &mut self.receiver {
+            if let &mut Some(ref mut rx) = &mut self.receiver {
                 rx.close();
                 // TODO: futures-0.2.0 introduces a try_recv method that is
                 // better to use here than poll.  Use it after upgrading to