f51ebcdb0f59e620505c5efc472d872f12739efe
Alan Somers <asomers@gmail.com>
2019-01-30 09:08:23 -0700

    Add a top-level example

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,28 @@
 // vim: tw=80
 
-//!  A library of [`Futures`]-aware locking primitives.  These locks can safely be
-//!  used in asynchronous environments like [`Tokio`].  When they block, they'll
-//!  only block a single task, not the entire reactor.
+//!  A library of [`Futures`]-aware locking primitives.  These locks can safely
+//!  be used in asynchronous environments like [`Tokio`].  When they block,
+//!  they'll only block a single task, not the entire reactor.
+//!
+//!  These primitives generally work much like their counterparts from the
+//!  standard library.  But instead of blocking, they return a `Future` that
+//!  completes when the lock has been acquired.
+//!
+//! # Examples
+//!
+//! ```
+//! # extern crate futures;
+//! # extern crate futures_locks;
+//! # use futures_locks::*;
+//! # use futures::executor::{Spawn, spawn};
+//! # use futures::Future;
+//! # fn main() {
+//! let mtx = Mutex::<u32>::new(0);
+//! let fut = mtx.lock().map(|mut guard| { *guard += 5; });
+//! spawn(fut).wait_future();
+//! assert_eq!(mtx.try_unwrap().unwrap(), 5);
+//! # }
+//! ```
 //!
 //! [`Futures`]: https://github.com/rust-lang-nursery/futures-rs
 //! [`Tokio`]: https:/tokio.rs
